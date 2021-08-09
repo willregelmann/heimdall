16673918b0ae269a59c980e68966bc1a81ccfe3a
Will Regelmann <will@regelmann.net>
2021-08-09 14:14:55 -0400

    Client 'get' returns only upgradable packages
    Added getall for up-to-date packages

--- client.py
+++ client.py
@@ -6,7 +6,7 @@ if len(sys.argv) == 1:
     print 'command must be provided'
     sys.exit()
 
-if sys.argv[1] in ['get', 'update']:
+if sys.argv[1] in ['get', 'getall', 'update']:
     hostlist = [sys.argv[2]] if len(sys.argv) >= 3 else config['hosts']
     for host in hostlist:
         try:
@@ -28,9 +28,10 @@ if sys.argv[1] in ['get', 'update']:
             s.close()    
             for module in data.keys():
                 for package in data[module]:
-                    if sys.argv[1] == 'get':
+                    if sys.argv[1] in ['get', 'getall']:
                         if package['current'] == package['available']:
-                            print '\033[0;32m%s %s %s (%s)\033[0m' % (host, module, package['name'], package['current'])
+                            if sys.argv[1] == 'getall':
+                                print '\033[0;32m%s %s %s (%s)\033[0m' % (host, module, package['name'], package['current'])
                         else:
                             print '\033[0;31m%s %s %s (%s => %s)\033[0m' % (host, module, package['name'], package['current'], package['available'])
                     elif sys.argv[1] == 'update':
